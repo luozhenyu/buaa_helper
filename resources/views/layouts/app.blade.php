50fcc74fa04b254b5fb8e442e3a1f743f1f82b46
luozhenyu <lzy@luozy.cn>
2017-06-02 09:58:27 +0800

    2017/06/02 09:57:55 CSS3动画

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,47 +5,24 @@
     #main_content {
         max-width: 100%;
         background-color: white;
-        padding-left: 12px;
-        padding-right: 12px;
-        padding-top: 15px;
-        padding-bottom: 15px;
+        padding: 15px 12px 15px 12px;
+
+        animation: fadeInto 0.6s;
     }
 
-    #main_content:empty, #crumb:empty {
-        display: none;
+    #crumb {
+        animation: fadeInto 0.3s;
     }
 
-    .alpha_hide {
-        filter: alpha(opacity=1);
-        -moz-opacity: 0.01;
-        opacity: 0.01;
+    @keyframes fadeInto {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
     }
 </style>
-@endpush
-
-@push('js')
-<script>
-    $(function () {
-        var ce = $("#crumb").is(":empty");
-        var me = $("#main_content").is(":empty");
-
-        if (!ce) {
-            $("#crumb").fadeTo(300, 1);
-            $("#crumb").removeClass("alpha_hide");
-            setTimeout(function () {
-                if (!me) {
-                    $("#main_content").fadeTo(200, 1);
-                    $("#main_content").removeClass("alpha_hide");
-                }
-            }, 100)
-        } else {
-            if (!me) {
-                $("#main_content").fadeTo(200, 1);
-                $("#main_content").removeClass("alpha_hide");
-            }
-        }
-    });
-</script>
 @endpush
 
 @section('content_full')
@@ -53,8 +30,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1 col-xs-12">
-                <ol class="breadcrumb alpha_hide" id="crumb">@stack("crumb")</ol>
-                <div id="main_content" class="jumbotron container alpha_hide">@yield('content')</div>
+                <ol class="breadcrumb" id="crumb">@stack("crumb")</ol>
+                <div id="main_content" class="jumbotron container">@yield('content')</div>
             </div>
         </div>
     </div>
